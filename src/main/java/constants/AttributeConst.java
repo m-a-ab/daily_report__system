d3315b39a14a61c041ab2842086fadae4bd251c1
自分の名前 <>
2021-12-26 19:50:32 +0900

    機能追加

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -48,7 +48,12 @@ public enum AttributeConst {
     REP_ID("id"),
     REP_DATE("report_date"),
     REP_TITLE("title"),
-    REP_CONTENT("content");
+    REP_CONTENT("content"),
+    REP_TIME_IN("timeIn"),
+    REP_TIME_OUT("timeOut");
+
+
+
 
     private final String text;
     private final Integer i;
